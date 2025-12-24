8cf8f25a95fb8081232778c81dcbfe47938b0a49
iq.dev.expert <forprivateuse3000@gmail.com>
2025-12-24 05:49:31 +0100

    up style body in css

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="flex flex-col min-h-screen items-center justify-center bg-purple-100">
+      <body className="items-center justify-center bg-purple-100">
         {children}
       </body>
     </html>
